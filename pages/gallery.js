468772a9710a9aac8a8c4335419ab1197ee0a14f
habzdafruzsina <56350697+habzdafruzsina@users.noreply.github.com>
2020-04-26 22:04:46 +0200

    Pics stored in indexedDB

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,28 +1,42 @@
-const photos = document.getElementById('photos');
+let photos = document.getElementById('photos');
 const fullScreen = document.getElementById('fullScreen');
 const infoBtn = document.getElementById('infoBtn');
 const backBtn = document.getElementById('backBtn');
 const deleteBtn = document.getElementById('deleteBtn');
-let database = JSON.parse(localStorage.getItem('database.json'));
+var fileNames;
 
 export async function load() {
-    var canvasText = "";
-    for(let i=0; i<database.length; i++){
-        canvasText += "<canvas id='" + database[i].fileName + "'></canvas>";
-    }
-    photos.innerHTML = canvasText;
-    console.log(photos.innerHTML);
-    
-    let canvases = photos.querySelectorAll(":scope > canvas");
-    canvases.forEach((canvas) => {
-        canvas.width = 180;
-        canvas.height = 180;
-        canvas.addEventListener('click', showPhoto)
+
+    getAllKeys(() => {
+        console.log(fileNames);
+
+        photos = document.getElementById('photos');
+        let canvases = photos.querySelectorAll(":scope > canvas");
+
+        if(canvases.length==0){
+                
+            var canvasText = "";
+            for(let i=0; i<fileNames.length; i++){
+                canvasText += "<canvas id='" + fileNames[i] + "'></canvas>";
+            }
+            photos.innerHTML = canvasText;
+            console.log(photos.innerHTML);
+            
+            canvases = photos.querySelectorAll(":scope > canvas");
+            console.log("lekértem a canvasokat: ");
+            console.log(canvases);
+            canvases.forEach((canvas) => {
+                canvas.width = 180;
+                canvas.height = 180;
+                canvas.addEventListener('click', showPhoto)
+            });
+            
+            for(let i=0; i<fileNames.length; i++){
+                loadImg(fileNames[i]);
+            }
+        }
+
     });
-    
-    for(let i=0; i<database.length; i++){
-        loadImg(i);
-    }
 
     deleteBtn.addEventListener("click", function() {
         document.getElementById("deleteModal").style.display = "block";
@@ -54,16 +68,8 @@ export async function load() {
     document.getElementById("deletebtn").addEventListener("click", function(){
         const src = fullScreen.getAttribute("data-src");
         console.log(src);
-        localStorage.removeItem(src);
 
-        let i = 0;
-        while(i<database.length && database[i].fileName != src){
-            i++;
-        }
-
-        database.splice(i, 1);
-        localStorage.removeItem('database.json');
-        localStorage.setItem('database.json', JSON.stringify(database));
+        deleteIndexedDb(src);
 
         let canvases = photos.querySelectorAll(":scope > canvas");
         canvases.forEach((canvas) => {
@@ -88,16 +94,16 @@ export async function load() {
     infoBtn.addEventListener("click", function() {
         const infoModal = document.getElementById("infoModal");
         infoModal.style.display = "block";
-        const info = searchForData(fullScreen.getAttribute("data-src"));
-
-        if(info){
+        const fileName = fullScreen.getAttribute("data-src");
+        findIndexedDB (fileName, (info) => {
             const infoBody = document.getElementById("infoBody");
             infoBody.innerHTML = "<p>Fájl név:   " +info.fileName+ "</p>" + 
             "<p>Latitude:   " +info.latitude+ "</p>" + 
             "<p>Longitude:   " +info.longitude+ "</p>" + 
             "<p>Dátum:   " +info.date+ "</p>" + 
             "<p>Megjegyzés:   " +info.comment+ "</p>";
-        }
+          }
+        );
     });
 
 
@@ -115,15 +121,18 @@ export async function load() {
 
 
 
-function loadImg(i){
-    var img = new Image();
-    img.src = window.localStorage.getItem(database[i].fileName);
-    img.onload = function(){
-        const canvas = document.getElementById(database[i].fileName);
-        const x = this.width/2 - canvas.width/2;
-        const y = this.height/2 - canvas.height/2;
-        canvas.getContext('2d').drawImage(img,-x,-y);
-    };
+function loadImg(fileName){
+    findIndexedDB (fileName, (data) => {
+        var img = new Image();
+        img.src = data.file;
+        img.onload = function(){
+            const canvas = document.getElementById(data.fileName);
+            console.log(canvas);
+            const x = this.width/2 - canvas.width/2;
+            const y = this.height/2 - canvas.height/2;
+            canvas.getContext('2d').drawImage(img,-x,-y);
+        };
+    })
 }
 
 function showPhoto(e){
@@ -133,14 +142,17 @@ function showPhoto(e){
     });
 
     const fileName = e.target.id;
-    var img = new Image();
-    img.src = window.localStorage.getItem(fileName);
-    img.onload = function(){
-        fullScreen.width = this.width;
-        fullScreen.height = this.height;
-        fullScreen.getContext('2d').drawImage(img,0,0);
-        fullScreen.setAttribute("data-src", fileName);
-    };
+
+    findIndexedDB (fileName, (data) => {
+        var img = new Image();
+        img.src = data.file;
+        img.onload = function(){
+            fullScreen.width = this.width;
+            fullScreen.height = this.height;
+            fullScreen.getContext('2d').drawImage(img,0,0);
+            fullScreen.setAttribute("data-src", data.fileName);
+        };
+    })
 
     fullScreen.style.display = 'block';
     infoBtn.style.display = 'block';
@@ -148,14 +160,76 @@ function showPhoto(e){
     deleteBtn.style.display = 'block';
 }
 
-function searchForData(id){
-    let i = 0;
-    while(i<database.length && database[i].fileName != id){
-        i++;
+
+function openIndexedDB () {
+    var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+    var openDB = indexedDB.open("PhotoTourStore", 1);
+  
+    openDB.onupgradeneeded = function() {
+      var db = {}
+      db.result = openDB.result;
+      db.store = db.result.createObjectStore("PhotoTourStore", {keyPath: "fileName"});
+    };
+  
+    return openDB;
+}
+  
+function getStoreIndexedDB (openDB) {
+    var db = {};
+    db.result = openDB.result;
+    db.tx = db.result.transaction("PhotoTourStore", "readwrite");
+    db.store = db.tx.objectStore("PhotoTourStore");
+    //request.onsuccess = function(event) {
+        return db;
+    //}
+}
+
+function findIndexedDB (fileName, callback) {
+    var openDB = openIndexedDB();
+
+    openDB.onsuccess = function() {
+        var db = getStoreIndexedDB(openDB);
+        var request = db.store.get(fileName);
+
+        request.onsuccess = function() {
+            callback(request.result);
+        };
+        db.tx.oncomplete = function() {
+            db.result.close();
+        };
     }
-    if(database[i].fileName == id){
-        return database[i];
-    } else {
-        return null;
+
+    return true;
+}
+
+function getAllKeys(callback){
+    var openDB = openIndexedDB();
+
+    openDB.onsuccess = function() {
+        var db = getStoreIndexedDB (openDB);
+        var request = db.store.getAllKeys();
+        request.onsuccess = function(event) {
+            fileNames = request.result;
+            callback();
+        };
+        db.tx.oncomplete = function() {
+            db.result.close();
+        };
     }
+
+    return true;
+}
+
+function deleteIndexedDb(fileName){
+    var openDB = openIndexedDB();
+
+    openDB.onsuccess = function() {
+        var db = getStoreIndexedDB (openDB);
+        var request = db.store.delete(fileName);
+        db.tx.oncomplete = function() {
+            db.result.close();
+        };
+    }
+    
+    return true;
 }
