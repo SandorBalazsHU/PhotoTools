1205d678de07ccf63e04dee58820ec2ec9007033
Sándor Balázs <it.sandor.balazs@gmail.com>
2020-05-16 14:43:59 +0200

    Loader repair

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 export async function load(widgetDatas) {
     const indexContent = document.querySelector("#index-content");
-    const response = await fetch(`widgets/widgets.html`);
+    const response = await fetch(`./widgets/widgets.html`);
     if (!response.ok) {
       return;
     }
     const html = await response.text();
     main.innerHTML = html;
-    (await import(`widgets/widgets.js`)).load();
+    (await import(`../widgets/widgets.js`)).load();
 }
